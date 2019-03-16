3d36b157de97569d7f353a2c2a25b891f5a756e3
eljalao <48590676+eljalao@users.noreply.github.com>
2019-03-16 15:04:15 +0800

    Update Brgy Calcu.R

--- Brgy Calcu.R
+++ Brgy Calcu.R
@@ -14,19 +14,7 @@ regionandarea = merge(x=area, y=regionnpopulation, by=c("Region"), all.y=TRUE)
 populationandarea = merge(x=population, y=regionandarea, by=c("Region"), all.y=TRUE)
 populationandarea$areaofbarangay = populationandarea$Area/populationandarea$countofbarangays
 populationandarea$densityofbarangay = populationandarea$Population/populationandarea$areaofbarangay
-
-
-##########By City
-citynpopulation =  group_by(population, Region)
-citynpopulation = summarize(citynpopulation, countofcities = n_distinct(CityProvince))
-
-cityandarea = merge(x=area, y=citynpopulation, by=c("Region"), all.y=TRUE)
-
-totalpopbycity = group_by(population, Region, CityProvince)
-totalpopbycity = summarize(totalpopbycity, populationofcity = sum(Population))
-
-
-populationandcityarea = merge(x=totalpopbycity, y=cityandarea, by=c("Region"), all.x=TRUE)
-populationandcityarea$areaofcity = populationandcityarea$Area/populationandcityarea$countofcities
-populationandcityarea$densityofcity = populationandcityarea$populationofcity/populationandcityarea$areaofcity
+populationandarea = arrange(populationandarea, desc(densityofbarangay) )
+populationandarea = populationandarea[1:5, ]
+write.csv(populationandarea, "brgydensities.csv")
 
