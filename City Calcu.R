4b6ceea11c63aba10e386607f795c948cdf32bb6
eljalao <48590676+eljalao@users.noreply.github.com>
2019-03-16 15:05:30 +0800

    Update City Calcu.R

--- City Calcu.R
+++ City Calcu.R
@@ -3,19 +3,6 @@ library(dplyr)
 area= read.csv("regionarea.csv")
 population= read.csv("population.csv")
 
-
-##########By Barangay
-regionnpopulation =  group_by(population, Region)
-regionnpopulation = summarize(regionnpopulation, countofbarangays = n())
-
-
-regionandarea = merge(x=area, y=regionnpopulation, by=c("Region"), all.y=TRUE)
-
-populationandarea = merge(x=population, y=regionandarea, by=c("Region"), all.y=TRUE)
-populationandarea$areaofbarangay = populationandarea$Area/populationandarea$countofbarangays
-populationandarea$densityofbarangay = populationandarea$Population/populationandarea$areaofbarangay
-
-
 ##########By City
 citynpopulation =  group_by(population, Region)
 citynpopulation = summarize(citynpopulation, countofcities = n_distinct(CityProvince))
@@ -29,4 +16,6 @@ totalpopbycity = summarize(totalpopbycity, populationofcity = sum(Population))
 populationandcityarea = merge(x=totalpopbycity, y=cityandarea, by=c("Region"), all.x=TRUE)
 populationandcityarea$areaofcity = populationandcityarea$Area/populationandcityarea$countofcities
 populationandcityarea$densityofcity = populationandcityarea$populationofcity/populationandcityarea$areaofcity
-
+populationandcityarea = arrange(populationandcityarea, desc(densityofcity) )
+populationandcityarea = populationandcityarea[1:5, ]
+write.csv(populationandcityarea, "brgydensities.csv")
